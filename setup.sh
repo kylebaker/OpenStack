4a01aa83bba4e17583f39bcffa2e15971419dab2
kylebaker <kylethomasbaker@gmail.com>
2019-10-26 13:33:01 -0600

    a

--- setup.sh
+++ setup.sh
@@ -57,6 +57,10 @@ function install_done {
   #reboot
 }
 
+### SCRIPT STARTS HERE ###
+
+### Check to see if you are
+
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
